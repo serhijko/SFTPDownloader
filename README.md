2670d3f6526ff16c65d694cd30db4438929e6e49
serhijko <sp_by@tut.by>
2021-11-05 22:58:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SFTPDownloader
+Java console application (maven project) for downloading files from a remote directory to a local one by SFTP
---
+++ src/main/java/by/sp/sftpdownloader/SFTPDownloader.java
@@ -0,0 +1,13 @@
+package by.sp.sftpdownloader;
+
+import java.util.HashMap;
+
+public class SFTPDownloader {
+
+    HashMap<String, String> settingsMap;
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+        }
+    }
+}
